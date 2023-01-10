f521694b285de0209dbd0f432e4d6b3ad632dc59
Ngugi <72761972+mark26745@users.noreply.github.com>
2023-01-10 10:22:58 +0300

    Changed some hyperlinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mark26745.github.io",
-  base:"/gevsite.github.io/",
+  site: "https://mark26745.github.io/gevsite.github.io/",
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
   }), mdx(), sitemap(), react()]
