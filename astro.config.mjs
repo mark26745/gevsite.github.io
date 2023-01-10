6b5cd52aae049a6a271ce132a7f0256b910ed615
Ngugi <72761972+mark26745@users.noreply.github.com>
2023-01-10 09:55:48 +0300

    modified config to host on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://mark26745.github.io",
+  base:"/gevsite.github.io/",
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
   }), mdx(), sitemap(), react()]
