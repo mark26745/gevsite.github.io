291a06c87339677519fc3030f0244dbeccd6394c
Ngugi <72761972+mark26745@users.noreply.github.com>
2023-01-10 13:15:50 +0300

    Added prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,13 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://mark26745.github.io",
   base: "/gevsite.github.io",
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), sitemap(), react()]
+  }), mdx(), sitemap(), react(), prefetch()]
 });
